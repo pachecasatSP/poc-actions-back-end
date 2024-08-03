e5e4e6f56a7b42dbed8d86a5b8f8e82afdb6a688
Adolfo Sales Pacheco <adolfo.pacheco@gmail.com>
2024-08-03 13:56:00 -0300

    ajuste

--- Domain/Entities/WeatherForeCast.cs
+++ Domain/Entities/WeatherForeCast.cs
@@ -1,6 +1,6 @@
 namespace bff_app_client.Server.Domain.Entities
 {
-    public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+    public record WeatherForecast(string Date, int TemperatureC, string? Summary)
     {
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
     }
