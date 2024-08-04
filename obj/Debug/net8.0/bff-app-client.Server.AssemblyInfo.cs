20c784ab9161bc0568bdd233598b8dc01f0fa293
Adolfo Sales Pacheco <adolfo.pacheco@gmail.com>
2024-08-04 14:27:41 -0300

    ajustes v2

--- obj/Debug/net8.0/bff-app-client.Server.AssemblyInfo.cs
+++ obj/Debug/net8.0/bff-app-client.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bff-app-client.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c06781f21dd6aa1c0e7e4ec652a8a6552e10794f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1754eff40fe6ecd56df5a5549fca4e988d36ac5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("bff-app-client.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bff-app-client.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- services/WeatherForeCastService.cs
+++ services/WeatherForeCastService.cs
@@ -6,34 +6,45 @@ namespace bff_app_client.Server.services
 {
     public class WeatherforecastService(IMemoryCache memoryCache) : IWeatherForecastService
     {
-        private IMemoryCache _memoryCache = memoryCache;
-        public const string CACHE_KEY = "LASTUSEDDATE";
+        private readonly string[] summaries = {
+                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
 
+        private IMemoryCache _memoryCache = memoryCache;
+        public const string CACHE_KEY_LASTUSEDDATE = "LASTUSEDDATE";
+        public const string CACHE_KEY_SENTFORECASTS = "SENTFORECASTS";
         public WeatherForecast[] Forecast(int count)
         {
-            DateOnly initialDate = DateOnly.FromDateTime(DateTime.Now);
+            DateTime initialDate = DateTime.Now;
             if (count == 1)
             {
-               initialDate = _memoryCache.Get<DateOnly>(CACHE_KEY);
+                initialDate = _memoryCache.Get<DateTime>(CACHE_KEY_LASTUSEDDATE);
+            }
+            else
+            {
+                var sentForecasts = _memoryCache.Get<IEnumerable<WeatherForecast>>($"{CACHE_KEY_SENTFORECASTS}_{count}");
+
+                if (sentForecasts != null)
+                {
+                    return sentForecasts.ToArray();
+                }
             }
 
-            var summaries = new[]
-            {
-                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-            };
-
+            int idGenerator = 0;
             var forecast = Enumerable.Range(1, count).Select(index =>
             new WeatherForecast
             (
-                initialDate.AddDays(index).ToString("dd/MM/yyyy"),
+                ++idGenerator,
+                DateOnly.FromDateTime(initialDate.AddDays(index)),
                 Random.Shared.Next(-20, 55),
                 summaries[Random.Shared.Next(summaries.Length)]
             ))
             .ToArray();
-            var lastUsedDate = forecast.Last().Date;
-            _memoryCache.Set<DateOnly>(CACHE_KEY, DateOnly.Parse(lastUsedDate));
 
+            var lastUsedDate = forecast.Last().Date.ToDateTime(TimeOnly.MinValue);
+            _memoryCache.Set(CACHE_KEY_LASTUSEDDATE, lastUsedDate);
 
+            _memoryCache.Set<IEnumerable<WeatherForecast>>($"{CACHE_KEY_SENTFORECASTS}_{count}", forecast);
             return forecast;
         }
     }
